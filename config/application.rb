b4b0712e4bab18da99746e63d7b86966aa53ee62
D Tucker <danielle@tuckerlabs.com>
2013-09-01 17:47:54 -0700

    Add assets precompile false for heroku https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting

--- config/application.rb
+++ config/application.rb
@@ -52,9 +52,9 @@ module PhotoGallery
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false #deploy to heroku https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
